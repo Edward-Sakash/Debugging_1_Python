c011183c24c2df7f525cb3b2067bcd605c7f9d37
Edward-Sakash <edward.sakash@yahoo.com>
2023-06-13 13:36:38 +0200

    done

--- task1.py
+++ task1.py
@@ -1,10 +1,13 @@
-def get_total(lst):
+numbers = [1,2,3,5]
+def get_total(lst, bla='hello'):
     index = 0
     total = 0
     while index < len(lst):
         total += lst[index]
         index += 1
-
+        
     print(total)
 
     return total
+
+print(get_total(numbers))
